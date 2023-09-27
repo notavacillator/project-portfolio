d0a8ddbcc874e162a8406dcb2f1cfeae7a3c1fa0
Aman Singh Bhadauria <bhadauria.shivam15@gmail.com>
2023-09-27 23:06:48 +0530

    react.email styled contact form email
    - added ContactFormEmail in email folder as the styled email using react.email
    - added React.createElement as property to the react key of the resend.emails.send as ts file can't understand jsx syntax of directly importing <ContactFormEmail>
    
    Co-Authored-By: Yuvraj Singh Bhadauria <74532212+Yuvraj2410@users.noreply.github.com>

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,6 +1,8 @@
 'use server'
 import { getErrorMessage, validateData } from '@/lib/utils';
 import { Resend } from 'resend';
+import ContactFormEmail from '@/email/contact-form-email';
+import React from 'react';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -32,8 +34,13 @@ export const sendEmail = async(formData : FormData) => {
         from: 'Contact form  <onboarding@resend.dev>',
         to: 'bhadauria.shivam15@gmail.com',
         subject: 'Message from contact form of portfolio.',
-        text: contactMessage as string,
-        reply_to: senderEmail as string
+        // text: contactMessage as string,
+        reply_to: senderEmail as string, 
+        react : React.createElement(ContactFormEmail, {
+          message: contactMessage as string, 
+          senderEmail : senderEmail as string, 
+        })
+        // react: <ContactFormEmail message={contactMessage} senderEmail={senderEmail}/>
       });
     } catch (error : unknown) {
       return {
--- email/contact-form-email.tsx
+++ email/contact-form-email.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+'use client'
 
-function ContactFormEmail() {
+import React from 'react'
+import {
+    Html,
+    Body,
+    Head, 
+    Heading,
+    Hr,
+    Container,
+    Preview,
+    Section,
+    Text
+} from '@react-email/components'
+import { Tailwind } from '@react-email/tailwind'
+
+type ContactFormEmailProps = {
+    message : string,
+    senderEmail : string, 
+}
+
+function ContactFormEmail({message, senderEmail } : ContactFormEmailProps) {
   return (
-    <div>ContactFormEmail</div>
+   <Html>
+    <Head/>
+    <Preview>New message from you portfolio site</Preview>
+    <Tailwind>
+        <Body>
+            <Container>
+                <Section>
+                    <Heading>You received the following message from the contact form</Heading>
+                    <Text>{message}</Text>
+                    <Hr/>
+                    <Text>The sender's email is : {senderEmail}</Text>
+                </Section>
+            </Container>
+        </Body>
+    </Tailwind>
+   </Html>
+    
   )
 }
 
