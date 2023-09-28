85ea680fd45ad962e3b441bddcddc4ec5fab4be6
Aman Singh Bhadauria <bhadauria.shivam15@gmail.com>
2023-09-28 16:59:22 +0530

    project component styled & adjusted for dark mode

--- components/project.tsx
+++ components/project.tsx
@@ -30,29 +30,30 @@ function Project({
         {   scale: scaleProgress, 
             opacity: opacityProgress
         }} 
-        className='group relative bg-gray-100 max-w-[42rem] border
-         border-black/5 overflow-hidden sm:pr-8 mb-3 sm:h-[20rem]
-         sm:mb-8 last:mb-0 even:sm:pl-8 even:pl-0 hover:bg-gray-200 transition py-4 rounded-lg'
+        className='group mb-3 sm:mb-8 last:mb-0'
     >
-        <div className='pt-4 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-1 
-        sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]'>
-            <h3 className='text-2xl font-semibold'>{title}</h3>
-            <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
-            <ul className='flex flex-wrap mt-4 gap-2  sm:mt-auto'>
-                {tags.map((tag, index) => (
-                    <li className = 'bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full' key={index}>{tag}</li>
-                ))}
-            </ul>
-        </div>
-        <Image src={imageUrl} alt='Project I worked on' quality={80} 
-        className='absolute top-8 -right-40 w-[28.25rem] 
-            rounded-t-lg shadow-2xl group-even:right-[initial] 
-            group-even:-left-40  group-hover:scale-105 group-hover:-translate-x-3
-            group-hover:translate-y-3 group-hover:-rotate-2 transition
+        <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 
+        relative sm:h-[25rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+            <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem] gap-2'>
+                <h3 className='text-2xl font-semibold'>{title}</h3>
+                <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/80'>{description}</p>
+                <ul className='flex flex-wrap mt-4 gap-2  sm:mt-auto'>
+                    {tags.map((tag, index) => (
+                        <li className = 'bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white dark:text-white/80 rounded-md'
+                         key={index}>{tag}</li>
+                    ))}
+                </ul>
+            </div>
+            <Image src={imageUrl} alt='Project I worked on' quality={80} 
+            className='absolute top-8 -right-40 w-[28.25rem] 
+                rounded-t-lg shadow-2xl group-even:right-[initial] 
+                group-even:-left-40  group-hover:scale-105 group-hover:-translate-x-3
+                group-hover:translate-y-3 group-hover:-rotate-2 transition
 
-            group-even:group-hover:translate-x-3
-            group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2
+                group-even:group-hover:translate-x-3
+                group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2
             '/>
+        </section>
     </motion.div> 
     )
 }
