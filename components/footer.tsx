42621229f1e34db9b07558936d553c21741b2014
Aman Singh Bhadauria <bhadauria.shivam15@gmail.com>
2023-09-28 14:00:14 +0530

    footer component completed

---
+++ components/footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='mb-10 px-4 text-center text-gray-500'>
+        <small className='block mb-2 text-xs'>
+            &copy; 2030 Ricardo. All rights reserved. 
+        </small>
+        <p className='text-xs'>
+            <span className='font-semibold'>About this website : </span> built with React & Next.js ( App router & Server Actions ), 
+            Typescript, Tailwind CSS, Framer Motion, Resend & Vercel hosting. 
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
