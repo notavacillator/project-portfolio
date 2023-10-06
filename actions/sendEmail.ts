1e543838c40c83b9b03a83d12ec588f020fdacb7
Aman Singh Bhadauria <36358196+notavacillator@users.noreply.github.com>
2023-10-06 18:11:21 +0530

    Update sendEmail.ts

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -6,7 +6,7 @@ import { Resend } from 'resend';
 import ContactFormEmail from '@/email/contact-form-email';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-
+console.log(process.env.RESEND_API_KEY);
 export const sendEmail = async(formData : FormData) => {
     const senderEmail = formData.get('senderEmail'); 
     const contactMessage = formData.get('contactMessage');
