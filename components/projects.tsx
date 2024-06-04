95d06792efd71598e854f6ef90b78004534f380e
Aman Singh Bhadauria <bhadauria.shivam15@gmail.com>
2024-06-05 01:16:23 +0530

    Update project.tsx to solve lint

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,30 +1,27 @@
-'use client'
+"use client";
 
-import React, {useEffect} from 'react'
-import SectionHeading from './section-heading'
-import { projectsData } from '@/lib/data'
-import Project from '@/components/project'
-import { useSectionInView } from '@/lib/hooks'
+import React, { useEffect } from "react";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import Project from "@/components/project";
+import { useSectionInView } from "@/lib/hooks";
 
 function Projects() {
-    const {ref} = useSectionInView('Projects', 0.1); 
-
-    
-  return (
-    <section ref={ref} id='projects' className='scroll-m-24 mb-10'>
-        <SectionHeading>My Projects</SectionHeading>
-        <div>
-            {
-                projectsData.map((project, index) => (
-                    <React.Fragment key = {index}>
-                        <Project {...project}/>
-                    </React.Fragment>
-                ))
-            }
-        </div>
-    </section>
-  )
+  //   const {ref} = useSectionInView('Projects', 0.1);
+  //     return (
+  //       <section ref={ref} id='projects' className='scroll-m-24 mb-10'>
+  //           <SectionHeading>My Projects</SectionHeading>
+  //           <div>
+  //               {
+  //                   projectsData.map((project, index) => (
+  //                       <React.Fragment key = {index}>
+  //                           <Project {...project}/>
+  //                       </React.Fragment>
+  //                   ))
+  //               }
+  //           </div>
+  //       </section>
+  //     )
 }
 
-
-export default Projects
+export default Projects;
